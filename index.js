136a3eb3988f027fe383c19da6523807c4a2aeb5
Noot <jidapar@bluebik.com>
2021-03-30 21:36:14 +0700

    fixed path not found

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index');
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
