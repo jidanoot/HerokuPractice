f1f74a6048238cb73bfb8f88737a35de150eace3
Noot <jidapar@bluebik.com>
2021-03-31 16:25:33 +0700

    can update point for each client

--- index.js
+++ index.js
@@ -15,79 +15,108 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 const io = socketio(server);
 
-
-// note = ['username', 'status', 'point', ['socket1', 'socket2']]
-var connected = [];
 var online = [];
-var list = []
-var answers_status = [];
 var answers_ranking = [];
-var point = [];
 var curr_point = [];
-var test = [1, 2, 3, 4]
 
-// function Register(username, socketid) {
-//     let existing_regis = online[3].indexOf(username)
-//     if () {
-//         console.log("true");
-//     } else {
-//         console.log("false");
-//     }
-// }
+function Register(username, socketid) {
+    if (online.length != 0) {
+        let existing_name = GetUserIndex(username);
+        if (existing_name > -1) {
+            online[existing_name][2].push(socketid);
+            curr_point.push(0);
+        } else {
+            let new_user = [username, 0, [socketid]];
+            online.push(new_user);
+            curr_point.push(0);
+        }
+    } else {
+        let new_user = [username, 0, [socketid]];
+        online.push(new_user);
+        curr_point.push(0);
+    }
+}
 
+function GetUserIndex(username) {
+    let existing_regis = -1;
+    for (index in online) {
+        existing_regis = online[index].indexOf(username)
+        if (existing_regis != -1) {
+            return index;
+        }
+    }
+    return existing_regis;
+}
 
+function GetUserIndexBySocket(user_socket) {
+    let existing_regis = -1;
+    for (index in online) {
+        existing_regis = online[index][2].indexOf(user_socket)
+        if (existing_regis != -1) {
+            return index;
+        }
+    }
+}
 
-// for socket
+function UpdateScore(user_socket, score) {
+    let check_user_socket = GetUserIndexBySocket(user_socket);
+    online[check_user_socket][1] = 1;
+    curr_point[check_user_socket] = score;
+    let existing = answers_ranking.indexOf(online[check_user_socket][0]);
+    if (existing > -1) {
+        answers_ranking.splice(existing, 1);
+        answers_ranking.push(online[check_user_socket][0]);
+    } else {
+        answers_ranking.push(online[check_user_socket][0]);
+    }
+}
+
+function ResetBoard() {
+    for (index in online) {
+        online[index][1] = 0;
+        curr_point[index] = 0;
+    }
+    answers_ranking = [];
+}
+
+function Disconnect(user_socketid) {
+    if (online.length > 0) {
+        let check = GetUserIndexBySocket(user_socketid);
+        if (check > -1) {
+            let index_user = GetUserIndexBySocket(user_socketid);
+            let index_socket = online[index_user][2].indexOf(user_socketid);
+            let index_ranking = answers_ranking.indexOf(online[index_user][0]);
+            if (index_ranking > -1) {
+                answers_ranking.splice(index_ranking, 1);
+            }
+            online[index_user][2].splice(index_socket, 1);
+            if (online[index_user][2].length < 1) {
+                online.splice(index_user, 3);
+            }
+        }
+    }
+}
+
 io.on('connection', (socket) => {
     console.log('a user connected');
-    connected.push(socket.id);
-    answers_status.push(0);
-    point.push(0);
-    io.sockets.emit('connect_room', { connected: connected, answers_status: answers_status, answers_ranking: answers_ranking });
-    // console.log('connect list ' + connected);
-    // console.log(socket.id + 'Client connected..');
+    socket.on('regist', (username) => {
+        Register(username, socket.id);
+        io.sockets.emit('connect_room', { online: online, answers_ranking: answers_ranking });
+    });
 
     socket.on('send_answer', (score) => {
-        // update score
-        let index = connected.indexOf(socket.id);
-        answers_status[index] = 1;
-        point[index] = score;
-        let existing = answers_ranking.indexOf(socket.id);
-        if (existing > -1) {
-            answers_ranking.splice(existing, 1);
-            answers_ranking.push(socket.id);
-        } else {
-            answers_ranking.push(socket.id);
-        }
-        io.sockets.emit('send_answer', { connected: connected, answers_status: answers_status, answers_ranking: answers_ranking });
-        console.log(socket.id + 'score input: ' + score);
+        UpdateScore(socket.id, score);
+        io.sockets.emit('send_answer', { online: online, answers_ranking: answers_ranking });
     });
 
     socket.on('reset', () => {
-        for (index in connected) {
-            answers_status[index] = 0;
-            point[index] = 0;
-        }
-        answers_ranking = [];
-        io.sockets.emit('reset', { connected: connected, answers_status: answers_status, answers_ranking: answers_ranking });
-        console.log('reset data');
+        ResetBoard();
+        io.sockets.emit('reset', { online: online, answers_ranking: answers_ranking });
     });
 
     socket.on('disconnect', () => {
-        console.log(socket.id + 'disconnect');
-        let index_connect = connected.indexOf(socket.id);
-        if (index_connect > -1) {
-            connected.splice(index_connect, 1);
-            answers_status.splice(index_connect, 1);
-            point.splice(index_connect, 1);
-        }
-        let index_ranking = answers_ranking.indexOf(socket.id);
-        if (index_ranking > -1) {
-            answers_ranking.splice(index_ranking, 1);
-        }
-
-        io.sockets.emit('connect_room', { connected: connected, answers_status: answers_status, answers_ranking: answers_ranking });
+        Disconnect(socket.id);
+        io.sockets.emit('connect_room', { online: online, answers_ranking: answers_ranking });
     });
-
 
 });
